cf7f15f0715d9a5c1fce3c1cad80609ac69b5017
Gabriel Pickard <gabriel.pickard@adcloud.com>
2012-03-31 13:52:03 +0200

    Reformatted, commented, removed slack in core.clj

--- src/herpderp/core.clj
+++ src/herpderp/core.clj
@@ -2,22 +2,9 @@
   (:refer-clojure :exclude [==])
   (:use clojure.core.logic))
 
-
-; (defn run-rule [rulename item]
-;   (let 
-;     [result (run 1 [q]
-;               (fresh [suc]
-;                 (rulename item suc) 
-;                 (== q {:item item :success suc})))]
-;     (first result)))
-
-
-; (defmacro defrule [rulename head & body]
-;   `(defna ~rulename [~head succ#]
-;     ([~head :pass] ~@body)
-;     ([~head :fail])))
-
-
+; -----------------------------------------------------------
+; shoveling in
+;
 ; for now: check all rules at every shovel-in (if so configured)
 ; later on: analyze to see when which kinds of facts about the state
 ;           are accessed, and make a map from those to their containing goals
@@ -27,11 +14,9 @@
 ;           hence make the (state-fact .. macro)
 ;           register its structures with that registry
 
-
 ; 
 ; one can define shovel-rules that all get called on the incoming shoveled item
 ; using doseq
-
 ; i bet this is some kind of inbuilt thing and has a real name:
 (defn giaku-foreach [funs item]
   "Applies a seq of functions to an item. Used for side-effects."
@@ -44,20 +29,16 @@
     (giaku-foreach shovels item)
     (map #(%) rules)))
 
+
+; --------------------------------------------------------------
+; defining rules
+
 ; a rule has a head/body construction
 ; or rather: 
 ; condition -> consequent
 
 ; these rules get checked on every shovel.
 ; they can rely on vanilla core.logic relations for their inferences
-
-; (defn _run-rule [head body]
-;   "Run a rule with head and body, where head and body are defne's."
-;   (run* [q]
-;     (fresh [r]
-;       (conda 
-;         [(head r) (body r) (== q {:data r :success :pass})]
-;         [(head r)          (== q {:data r :success :fail})]))))
 
 (defmacro defrule [nom [& varvec] head-expr body-expr]
   "A rule is a logical implication: head-expr => body-expr.
@@ -70,9 +51,6 @@
             [~head-expr            (== q# {:data [~@varvec] :success :fail})])))
     ; TODO: also register the rule with the suite
     ))
-
-; TODO: consider actually not to put in head/body in as defne's, rather leave them as expressions
-; because it's stupid that head and tail can't share vars? actually they can't anyway (?), just by the hierarchical structure. so maybe it's ok?
 
 (def rule-not-tbl
   {:pass :fail :fail :pass})
